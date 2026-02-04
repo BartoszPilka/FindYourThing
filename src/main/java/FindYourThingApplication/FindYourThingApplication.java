7c2d03755ccc9ed66be80d9dabab1a0daa402a4a
BartoszPilka <bartoszpilka02@gmail.com>
2026-02-04 12:13:36 +0100

    Project files added

---
+++ src/main/java/FindYourThingApplication/FindYourThingApplication.java
@@ -0,0 +1,13 @@
+package FindYourThingApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindYourThingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindYourThingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FindYourThingApplication/FindYourThingApplicationTests.java
@@ -0,0 +1,13 @@
+package FindYourThingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindYourThingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
